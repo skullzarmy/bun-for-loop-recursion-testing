262abf56c47bbf7ddc8c838a3947e102a21e4ad1
skllzrmy.tez <joe@poundfit.com>
2023-11-11 21:37:01 -0800

    updating report results

--- report.py
+++ report.py
@@ -1,169 +1,74 @@
 import json
 import glob
 from statistics import mean
+
 import matplotlib.pyplot as plt
 
 def load_test_results():
     """
     Load test results from all JSON files that match the pattern 'test_results_*.json'.
-    
-    Returns:
-        results_by_env (dict): A dictionary containing lists of test results, grouped by environment.
+    Returns a dictionary containing lists of test results, grouped by environment.
     """
     results_by_env = {'bun.sh': [], 'node.js': []}
     for file_name in glob.glob('test_results_*.json'):
         try:
             with open(file_name, 'r') as file:
                 data = json.load(file)
-                # Default to 'bun.sh' if environment not specified
                 environment = data.get('environment', 'bun.sh')
-                # Aggregate results in the appropriate environment category
                 results_by_env.setdefault(environment, []).extend(data.get('test_results', {}).values())
         except (json.JSONDecodeError, FileNotFoundError, KeyError, EOFError) as e:
             print(f"Error loading {file_name}: {e}")
             continue
     return results_by_env
 
-def analyze_results(results_by_env):
-    """
-    Analyze test results for each environment and create a master report comparing environments.
-
-    Parameters:
-        results_by_env (dict): A dictionary containing lists of test results, grouped by environment.
-    """
-    # Analyze and create individual reports for each environment
-    depth_data_by_env = {}
-    for env, results in results_by_env.items():
-        depth_data = analyze_environment(results, env)
-        depth_data_by_env[env] = depth_data
-
-    # Create a master report comparing environments
-    create_master_report(depth_data_by_env)
-
 def analyze_environment(results, environment):
     """
     Analyze test results for a specific environment and generate a report and graphs.
-
-    Parameters:
-        results (list): A list of dictionaries containing the test results for an environment.
-        environment (str): The environment being analyzed.
-
-    Returns:
-        depth_data (dict): A dictionary containing aggregated test metrics for the environment.
     """
     depth_data = {}
     for tests in results:
         for test in tests:
             depth = test['depth']
-            metrics = depth_data.setdefault(depth, {'times': [], 'memories': []})
+            metrics = depth_data.setdefault(depth, {'times': [], 'memories': [], 'results': []})
             metrics['times'].append(test['executionTime'])
             metrics['memories'].append(test['memoryUsage'])
-
-    report_filename = f'test_analysis_report_{environment}.md'
-    with open(report_filename, 'w') as report_file:
-        write_report(report_file, depth_data, environment)
-
+            metrics['results'].append(test)
+    generate_report(depth_data, environment)
     plot_graphs(depth_data, environment)
     return depth_data
 
-def create_master_report(depth_data_by_env):
+def generate_report(depth_data, environment):
     """
-    Create a master report comparing test results across different environments.
-
-    Parameters:
-        depth_data_by_env (dict): A dictionary of test metrics grouped by environment.
+    Generate a markdown report file with a detailed analysis of the test results.
     """
-    master_report_filename = 'test_analysis_master_report.md'
-    with open(master_report_filename, 'w') as master_report:
-        master_report.write("# Master Test Analysis Report\n\n")
-        master_report.write("## Comparison of Environments\n\n")
-        master_report.write("![Comparison Graphs](./test_analysis_comparison_graphs.png)\n\n")
-
-        for depth in sorted(next(iter(depth_data_by_env.values())).keys()):
-            master_report.write(f"### Depth {depth}\n")
-            for env, depth_data in depth_data_by_env.items():
-                if depth in depth_data:
-                    metrics = depth_data[depth]
-                    master_report.write(f"- Environment: {env}\n")
-                    master_report.write(f"  - Mean Execution Time: {mean(metrics['times']):.4f} ms\n")
-                    master_report.write(f"  - Mean Memory Usage: {mean(metrics['memories']):.4f} MB\n")
-
-            master_report.write("\n")
-
-    plot_comparison_graphs(depth_data_by_env)
-
-def plot_comparison_graphs(depth_data_by_env):
-    """
-    Plot comparison graphs for average execution time and memory usage across environments.
-
-    Parameters:
-        depth_data_by_env (dict): A dictionary of test metrics grouped by environment.
-    """
-    environments = depth_data_by_env.keys()
-    all_depths = sorted(set(depth for depth_data in depth_data_by_env.values() for depth in depth_data.keys()))
-
-    plt.figure(figsize=(12, 6), dpi=75)
-
-    # Execution Time Comparison
-    plt.subplot(1, 2, 1)
-    for env, depth_data in depth_data_by_env.items():
-        avg_times = [mean(depth_data[depth]['times']) if depth in depth_data else None for depth in all_depths]
-        plt.plot(all_depths, avg_times, marker='o', linestyle='--', label=env)
-    plt.yscale('log')
-    plt.title('Execution Time Comparison by Depth')
-    plt.xlabel('Depth')
-    plt.ylabel('Average Execution Time (ms)')
-    plt.legend()
-
-    # Memory Usage Comparison
-    plt.subplot(1, 2, 2)
-    for env, depth_data in depth_data_by_env.items():
-        avg_memories = [mean(depth_data[depth]['memories']) if depth in depth_data else None for depth in all_depths]
-        plt.plot(all_depths, avg_memories, marker='o', label=env)
-    plt.title('Memory Usage Comparison by Depth')
-    plt.xlabel('Depth')
-    plt.ylabel('Average Memory Usage (MB)')
-    plt.legend()
-
-    plt.tight_layout()
-    plt.savefig('test_analysis_comparison_graphs.png')
-
-def write_report(report_file, depth_data, environment):
-    """
-    Write a markdown report file with a detailed analysis of the test results for a specific environment.
-
-    Parameters:
-        report_file (file object): The file object to write the report to.
-        depth_data (dict): A dictionary containing test metrics for an environment.
-        environment (str): The environment being analyzed.
-    """
-    graph_filename = f'./test_analysis_graphs_{environment}.png'
-    
-    report_file.write(f"# Test Analysis Report for {environment}\n\n")
-    report_file.write(f"![Graphs for {environment}]({graph_filename})\n\n")
-    report_file.write("## Test Results Summary\n\n")
-    report_file.write("| Depth | Average Time (ms) | Average Memory Usage (MB) |\n| --- | --- | --- |\n")
-    for depth, metrics in depth_data.items():
-        report_file.write(f"| {depth} | {mean(metrics['times']):.4f} | {mean(metrics['memories']):.4f} |\n")
-
-    report_file.write("\n## Detailed Results\n\n")
-    for depth, metrics in depth_data.items():
-        report_file.write(f"### Depth {depth}\n")
-        report_file.write(f"- Execution Time: Mean: {mean(metrics['times']):.4f} ms\n")
-        report_file.write(f"- Memory Usage: Mean: {mean(metrics['memories']):.4f} MB\n\n")
+    report_filename = f'test_analysis_report_{environment}.md'
+    with open(report_filename, 'w') as report_file:
+        report_file.write(f"# Test Analysis Report for {environment}\n\n")
+        report_file.write(f"![Graphs for {environment}](test_analysis_graphs_{environment}.png)\n\n")
+        report_file.write("## Test Results Summary\n\n")
+        for depth, metrics in depth_data.items():
+            report_file.write(f"### Depth {depth}\n")
+            report_file.write(f"- Execution Time:\n")
+            report_file.write(f"    - Mean: {mean(metrics['times']):.4f} ms\n")
+            report_file.write(f"    - Max: {max(metrics['times']):.4f} ms\n")
+            report_file.write(f"    - Min: {min(metrics['times']):.4f} ms\n")
+            report_file.write(f"- Memory Usage:\n")
+            report_file.write(f"    - Mean: {mean(metrics['memories']):.4f} MB\n")
+            report_file.write(f"    - Max: {max(metrics['memories']):.4f} MB\n")
+            report_file.write(f"    - Min: {min(metrics['memories']):.4f} MB\n\n")
+            report_file.write("<details>\n<summary>Detailed Test Results</summary>\n\n")
+            report_file.write("| Test | Execution Time (ms) | Memory Usage (MB) |\n| --- | --- | --- |\n")
+            for result in metrics['results']:
+                report_file.write(f"| Test at Depth {depth} | {result['executionTime']:.4f} | {result['memoryUsage']:.4f} |\n")
+            report_file.write("</details>\n\n")
 
 def plot_graphs(depth_data, environment):
     """
-    Plot graphs for the average execution time and memory usage by depth for a specific environment.
-
-    Parameters:
-        depth_data (dict): A dictionary containing test metrics for an environment.
-        environment (str): The environment being analyzed.
+    Plot graphs for the average execution time and memory usage by depth.
     """
     depths = list(depth_data.keys())
     avg_times = [mean(metrics['times']) for metrics in depth_data.values()]
     avg_memories = [mean(metrics['memories']) for metrics in depth_data.values()]
-
     plt.figure(figsize=(12, 6), dpi=75)
     plt.subplot(1, 2, 1)
     plt.plot(depths, avg_times, marker='o', color='k', linestyle='--', linewidth=2, markersize=6)
@@ -171,21 +76,73 @@ def plot_graphs(depth_data, environment):
     plt.title(f'Execution Time by Depth - {environment}')
     plt.xlabel('Depth')
     plt.ylabel('Average Execution Time (ms)')
-
     plt.subplot(1, 2, 2)
     plt.plot(depths, avg_memories, marker='o', color='r')
     plt.title(f'Memory Usage by Depth - {environment}')
     plt.xlabel('Depth')
     plt.ylabel('Average Memory Usage (MB)')
-
     plt.tight_layout()
     plt.savefig(f'test_analysis_graphs_{environment}.png')
+
+def create_master_report(depth_data_by_env):
+    """
+    Create a master report comparing test results across different environments.
+    """
+    master_report_filename = 'test_analysis_master_report.md'
+    with open(master_report_filename, 'w') as master_report:
+        master_report.write("# Master Test Analysis Report\n\n")
+        master_report.write("## Comparison of Environments\n\n")
+        master_report.write("![Comparison Graphs](test_analysis_comparison_graphs.png)\n\n")
+        for depth in sorted(next(iter(depth_data_by_env.values())).keys()):
+            master_report.write(f"### Depth {depth}\n")
+            for env, depth_data in depth_data_by_env.items():
+                metrics = depth_data.get(depth, {'times': [], 'memories': []})
+                master_report.write(f"- Environment: {env}\n")
+                master_report.write(f"  - Execution Time: Mean: {mean(metrics['times']):.4f} ms, Max: {max(metrics['times']):.4f} ms, Min: {min(metrics['times']):.4f} ms\n")
+                master_report.write(f"  - Memory Usage: Mean: {mean(metrics['memories']):.4f} MB, Max: {max(metrics['memories']):.4f} MB, Min: {min(metrics['memories']):.4f} MB\n\n")
+
+def plot_comparison_graphs(depth_data_by_env):
+    """
+    Plot comparison graphs for average execution time and memory usage across environments.
+    """
+    plt.figure(figsize=(12, 6), dpi=75)
+    plt.subplot(1, 2, 1)
+    for env, depth_data in depth_data_by_env.items():
+        depths = list(depth_data.keys())
+        avg_times = [mean(metrics['times']) for metrics in depth_data.values()]
+        plt.plot(depths, avg_times, marker='o', linestyle='--', label=env)
+    plt.yscale('log')
+    plt.title('Execution Time Comparison by Depth')
+    plt.xlabel('Depth')
+    plt.ylabel('Average Execution Time (ms)')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    for env, depth_data in depth_data_by_env.items():
+        depths = list(depth_data.keys())
+        avg_memories = [mean(metrics['memories']) for metrics in depth_data.values()]
+        plt.plot(depths, avg_memories, marker='o', label=env)
+    plt.title('Memory Usage Comparison by Depth')
+    plt.xlabel('Depth')
+    plt.ylabel('Average Memory Usage (MB)')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('test_analysis_comparison_graphs.png')
+
+def analyze_results(results_by_env):
+    """
+    Analyze test results for each environment and create a master report comparing environments.
+    """
+    depth_data_by_env = {}
+    for env, results in results_by_env.items():
+        depth_data = analyze_environment(results, env)
+        depth_data_by_env[env] = depth_data
+    create_master_report(depth_data_by_env)
+    plot_comparison_graphs(depth_data_by_env)
 
 def main():
     """
     Main execution function of the script.
-
-    Loads test results, analyzes them by environment, generates individual reports, and creates a master report for comparison.
+    Loads test results, analyzes them by environment, and generates reports and comparison graphs.
     """
     results_by_env = load_test_results()
     analyze_results(results_by_env)
--- test_analysis_master_report.md
+++ test_analysis_master_report.md
@@ -1,96 +1,87 @@
 # Master Test Analysis Report
 
-### Bun.sh
-
-[test_analysis_report_bun.sh.md](./test_analysis_report_bun.sh.md)
-
-### Node.js
-
-[test_analysis_report_node.js.md](./test_analysis_report_node.js.md)
-
----
-
 ## Comparison of Environments
 
-![Comparison Graphs](./test_analysis_comparison_graphs.png)
+![Comparison Graphs](test_analysis_comparison_graphs.png)
 
 ### Depth 1
+- Environment: bun.sh
+  - Execution Time: Mean: 0.0161 ms, Max: 1.3009 ms, Min: 0.0011 ms
+  - Memory Usage: Mean: 1.4089 MB, Max: 11.7891 MB, Min: 0.2077 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 0.0161 ms
-    -   Mean Memory Usage: 1.4089 MB
--   Environment: node.js
-    -   Mean Execution Time: 0.0142 ms
-    -   Mean Memory Usage: 5.8652 MB
+- Environment: node.js
+  - Execution Time: Mean: 0.0142 ms, Max: 4.8753 ms, Min: 0.0021 ms
+  - Memory Usage: Mean: 5.8652 MB, Max: 22.5322 MB, Min: 3.6697 MB
 
 ### Depth 2
+- Environment: bun.sh
+  - Execution Time: Mean: 0.0337 ms, Max: 1.9965 ms, Min: 0.0100 ms
+  - Memory Usage: Mean: 1.5775 MB, Max: 11.7891 MB, Min: 0.2077 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 0.0337 ms
-    -   Mean Memory Usage: 1.5775 MB
--   Environment: node.js
-    -   Mean Execution Time: 0.0392 ms
-    -   Mean Memory Usage: 6.0247 MB
+- Environment: node.js
+  - Execution Time: Mean: 0.0392 ms, Max: 6.0300 ms, Min: 0.0174 ms
+  - Memory Usage: Mean: 6.0247 MB, Max: 22.5375 MB, Min: 3.6746 MB
 
 ### Depth 3
+- Environment: bun.sh
+  - Execution Time: Mean: 0.1766 ms, Max: 7.3989 ms, Min: 0.0996 ms
+  - Memory Usage: Mean: 1.7949 MB, Max: 11.7891 MB, Min: 0.2077 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 0.1766 ms
-    -   Mean Memory Usage: 1.7949 MB
--   Environment: node.js
-    -   Mean Execution Time: 0.2448 ms
-    -   Mean Memory Usage: 6.2239 MB
+- Environment: node.js
+  - Execution Time: Mean: 0.2448 ms, Max: 7.6203 ms, Min: 0.1706 ms
+  - Memory Usage: Mean: 6.2239 MB, Max: 22.5811 MB, Min: 3.7179 MB
 
 ### Depth 4
+- Environment: bun.sh
+  - Execution Time: Mean: 1.2596 ms, Max: 7.4257 ms, Min: 0.9936 ms
+  - Memory Usage: Mean: 2.0944 MB, Max: 11.7891 MB, Min: 0.2077 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 1.2596 ms
-    -   Mean Memory Usage: 2.0944 MB
--   Environment: node.js
-    -   Mean Execution Time: 2.0537 ms
-    -   Mean Memory Usage: 6.3826 MB
+- Environment: node.js
+  - Execution Time: Mean: 2.0537 ms, Max: 5.1085 ms, Min: 1.7086 ms
+  - Memory Usage: Mean: 6.3826 MB, Max: 20.5360 MB, Min: 3.7166 MB
 
 ### Depth 5
+- Environment: bun.sh
+  - Execution Time: Mean: 12.2534 ms, Max: 37.3940 ms, Min: 10.2269 ms
+  - Memory Usage: Mean: 2.4069 MB, Max: 11.7891 MB, Min: 0.2077 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 12.2534 ms
-    -   Mean Memory Usage: 2.4069 MB
--   Environment: node.js
-    -   Mean Execution Time: 18.1426 ms
-    -   Mean Memory Usage: 6.5366 MB
+- Environment: node.js
+  - Execution Time: Mean: 18.1426 ms, Max: 36.0605 ms, Min: 17.2098 ms
+  - Memory Usage: Mean: 6.5366 MB, Max: 23.6899 MB, Min: 3.7286 MB
 
 ### Depth 6
+- Environment: bun.sh
+  - Execution Time: Mean: 122.6465 ms, Max: 261.2737 ms, Min: 107.7531 ms
+  - Memory Usage: Mean: 2.5624 MB, Max: 11.7910 MB, Min: 1.8468 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 122.6465 ms
-    -   Mean Memory Usage: 2.5624 MB
--   Environment: node.js
-    -   Mean Execution Time: 187.1683 ms
-    -   Mean Memory Usage: 6.9020 MB
+- Environment: node.js
+  - Execution Time: Mean: 187.1683 ms, Max: 254.2883 ms, Min: 175.7874 ms
+  - Memory Usage: Mean: 6.9020 MB, Max: 22.4504 MB, Min: 3.6045 MB
 
 ### Depth 7
+- Environment: bun.sh
+  - Execution Time: Mean: 1232.1892 ms, Max: 2029.7771 ms, Min: 1099.7665 ms
+  - Memory Usage: Mean: 3.3887 MB, Max: 11.7997 MB, Min: 1.8635 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 1232.1892 ms
-    -   Mean Memory Usage: 3.3887 MB
--   Environment: node.js
-    -   Mean Execution Time: 1899.1045 ms
-    -   Mean Memory Usage: 7.8682 MB
+- Environment: node.js
+  - Execution Time: Mean: 1899.1045 ms, Max: 2035.3865 ms, Min: 1808.4117 ms
+  - Memory Usage: Mean: 7.8682 MB, Max: 21.6263 MB, Min: 3.6685 MB
 
 ### Depth 8
+- Environment: bun.sh
+  - Execution Time: Mean: 15204.0159 ms, Max: 20507.9253 ms, Min: 12590.3901 ms
+  - Memory Usage: Mean: 5.4116 MB, Max: 11.8899 MB, Min: 1.9528 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 15204.0159 ms
-    -   Mean Memory Usage: 5.4116 MB
--   Environment: node.js
-    -   Mean Execution Time: 18316.1047 ms
-    -   Mean Memory Usage: 11.1337 MB
+- Environment: node.js
+  - Execution Time: Mean: 18316.1047 ms, Max: 18767.6704 ms, Min: 18162.5968 ms
+  - Memory Usage: Mean: 11.1337 MB, Max: 23.0882 MB, Min: 4.3178 MB
 
 ### Depth 9
+- Environment: bun.sh
+  - Execution Time: Mean: 163026.9187 ms, Max: 202189.5296 ms, Min: 127639.2988 ms
+  - Memory Usage: Mean: 7.8056 MB, Max: 12.7907 MB, Min: 2.8332 MB
 
--   Environment: bun.sh
-    -   Mean Execution Time: 163026.9187 ms
-    -   Mean Memory Usage: 7.8056 MB
--   Environment: node.js
-    -   Mean Execution Time: 183032.4489 ms
-    -   Mean Memory Usage: 15.0055 MB
+- Environment: node.js
+  - Execution Time: Mean: 183032.4489 ms, Max: 184732.0773 ms, Min: 181941.7209 ms
+  - Memory Usage: Mean: 15.0055 MB, Max: 22.5308 MB, Min: 7.8163 MB
+
